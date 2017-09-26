0ca25b6160a56f1107a8d0079c25495881891740
jz267 <spsp713108@gmail.com>
2017-09-26 21:48:03 +1300

    Create another button in main screen to jump to high score screen. Fix the bug that the GamePlay screen is not full screen. Estimated Time: 10 mins. Actual Time: 12 mins. Number of Lines of Code: 50lines

--- app/src/main/java/com/example/a17255/a8project/MainActivity.java
+++ app/src/main/java/com/example/a17255/a8project/MainActivity.java
@@ -9,20 +9,30 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button start_play_button;
+    Button high_score_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Link the button using its ID, and set onClick listener to make it jump to game screen once
+//        Link the button using its ID, and set onClick listener to make it jump to other screen once
 //        the user click on the button
         start_play_button = (Button) findViewById(R.id.start_play_button);
+        high_score_button = (Button) findViewById(R.id.high_score_button);
 
         start_play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GamePlay.class);
+                startActivity(intent);
+            }
+        });
+
+        high_score_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HighScore.class);
                 startActivity(intent);
             }
         });

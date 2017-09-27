1015c41483bd2a786d91aeb74606a17716500d7c
jz267 <spsp713108@gmail.com>
2017-09-27 21:51:52 +1300

    Draw a red ball in the gameplay screen Estimated Time: 10 mins. Actual Time: 11 mins. Number of Lines of Code: 30lines

--- app/src/main/java/com/example/a17255/a8project/GamePlay.java
+++ app/src/main/java/com/example/a17255/a8project/GamePlay.java
@@ -1,10 +1,40 @@
 package com.example.a17255.a8project;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 
 public class GamePlay extends AppCompatActivity {
+
+
+//    Create my own view class
+    class MyCanvas extends View {
+
+        public MyCanvas(Context context) {
+            super(context);
+        }
+
+//        Draw a red ball
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            canvas.drawCircle(100, 100, 50, paint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            return super.onTouchEvent(event);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +46,8 @@ public class GamePlay extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
-        setContentView(R.layout.activity_game_play);
+//        setContentView(R.layout.activity_game_play);
+        setContentView(new MyCanvas(this));
     }
+
 }

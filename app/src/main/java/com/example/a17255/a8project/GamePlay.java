013f8bbbf9af7d7841a9e8a196091386a450ac91
Jiaming Zhang <spsp713108@gmail.com>
2017-10-09 16:56:00 +1300

    Create the target and win after you hit the target and jump to the high score page with the score inside. Estimated Time: 120 mins. Actual Time: 50mins. Number of Lines of Code: 50 lines

--- app/src/main/java/com/example/a17255/a8project/GamePlay.java
+++ app/src/main/java/com/example/a17255/a8project/GamePlay.java
@@ -1,9 +1,12 @@
 package com.example.a17255.a8project;
 
+import android.app.DialogFragment;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,9 +15,12 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 public class GamePlay extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    FragmentManager fragmentManager;
+    private final String FRAGMENT_TAG = "fragmentTag";
+    WinDialogFragment winDialogFragment;
 
 
     float mCurrentX, mCurrentY;
@@ -24,6 +30,7 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
     float obstacleX2 = 550, obstacleY2 = 400;
     float obstacleX3 = 280, obstacleY3 = 400;
     float obstacleX4 = 550, obstacleY4 = 700;
+    float targetX = 400, targetY = 100;
 
     MyCanvas myCanvas;
 
@@ -91,11 +98,13 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
         private Paint obstaclePaint2;
         private Paint obstaclePaint3;
         private Paint obstaclePaint4;
+        private Paint targetPaint;
 
 
         private final int BALL_RADIUS = 50;
         private final int OBSTACLE_RADIUS_1 = 80;
         private final int OBSTACLE_RADIUS_2 = 40;
+        private final int TARGET_RADIUS = 20;
 
         public MyCanvas(Context context) {
             super(context);
@@ -114,6 +123,9 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
             obstaclePaint4 = new Paint();
             obstaclePaint3.setColor(Color.DKGRAY);
 
+            targetPaint = new Paint();
+            targetPaint.setColor(Color.GREEN);
+
         }
 
 
@@ -123,11 +135,12 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-//            draw four obstacles
+//            draw four obstacles and one target
             drawObstacle1(canvas, obstacleX1, obstacleY1, obstaclePaint1);
             drawObstacle1(canvas, obstacleX2, obstacleY2, obstaclePaint2);
             drawObstacle2(canvas, obstacleX3, obstacleY3, obstaclePaint3);
             drawObstacle2(canvas, obstacleX4, obstacleY4, obstaclePaint4);
+            drawTarget(canvas, targetX, targetY, targetPaint);
 
             if (currentSpeedX == 0 && currentSpeedY == 0) {
                 drawBall(canvas);
@@ -189,13 +202,13 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
 //                    slow down when collide
                     currentSpeedX = Math.abs(currentSpeedX) - 1000;
                     currentSpeedY = Math.abs(currentSpeedY) - 1000;
-//                    stop compelete
-                    if (currentSpeedX < 1000) {
-                        currentSpeedX = 0;
-                    }
-                    if (currentSpeedY < 1000) {
-                        currentSpeedY = 0;
-                    }
+////                    stop compelete
+//                    if (currentSpeedX < 1000) {
+//                        currentSpeedX = 0;
+//                    }
+//                    if (currentSpeedY < 1000) {
+//                        currentSpeedY = 0;
+//                    }
                     obstaclePaint3.setColor(Color.CYAN);
                 }
                 if (Math.pow(mCurrentX - obstacleX4, 2) + Math.pow(mCurrentY - obstacleY4, 2) <=
@@ -205,13 +218,13 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
 //                    slow down when collide
                     currentSpeedX = Math.abs(currentSpeedX) - 1000;
                     currentSpeedY = Math.abs(currentSpeedY) - 1000;
-//                    stop compelete
-                    if (currentSpeedX < 1000) {
-                        currentSpeedX = 0;
-                    }
-                    if (currentSpeedY < 1000) {
-                        currentSpeedY = 0;
-                    }
+////                    stop compelete
+//                    if (currentSpeedX < 1000) {
+//                        currentSpeedX = 0;
+//                    }
+//                    if (currentSpeedY < 1000) {
+//                        currentSpeedY = 0;
+//                    }
                     obstaclePaint4.setColor(Color.CYAN);
                 }
                 if (Math.pow(mCurrentX - obstacleX3, 2) + Math.pow(mCurrentY - obstacleY3, 2) >
@@ -223,13 +236,30 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
                     obstaclePaint4.setColor(Color.DKGRAY);
                 }
 
+
+//                detect the collision between the ball and the target
+                if (Math.pow(mCurrentX - targetX, 2) + Math.pow(mCurrentY - targetY, 2) <=
+                        Math.pow(BALL_RADIUS + TARGET_RADIUS, 2)) {
+//                    change score when collide
+                    currentSpeedX = 0;
+                    currentSpeedY = 0;
+                    score += 200;
+                    confirmFireMissiles();
+
+                }
+
+
+
+
+
+
 //                display score
                 drawScore(canvas);
 
 
 //                draw ball after fling
-                mCurrentX = mCurrentX + currentSpeedX / 7000 * DURATION_PER_FRAME_IN_MS;
-                mCurrentY = mCurrentY + currentSpeedY / 7000 * DURATION_PER_FRAME_IN_MS;
+                mCurrentX = mCurrentX + currentSpeedX / 5000 * DURATION_PER_FRAME_IN_MS;
+                mCurrentY = mCurrentY + currentSpeedY / 5000 * DURATION_PER_FRAME_IN_MS;
                 drawBall(canvas);
                 postInvalidateDelayed(DURATION_PER_FRAME_IN_MS);
 
@@ -250,6 +280,10 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
 
         private void drawObstacle2(Canvas canvas, float x, float y, Paint paint) {
             canvas.drawCircle(x, y, OBSTACLE_RADIUS_2, paint);
+        }
+
+        private void drawTarget(Canvas canvas, float x, float y, Paint paint) {
+            canvas.drawCircle(x, y, TARGET_RADIUS, paint);
         }
 
         @Override
@@ -279,5 +313,14 @@ public class GamePlay extends AppCompatActivity implements GestureDetector.OnGes
 
         gestureDetectorCompat = new GestureDetectorCompat(this, this);
     }
+
+
+    public void confirmFireMissiles() {
+        DialogFragment newFragment = new WinDialogFragment(GamePlay.this, score);
+        newFragment.show(getFragmentManager(), "win");
+    }
+
+
+
 
 }
---
+++ app/src/main/java/com/example/a17255/a8project/WinDialogFragment.java
@@ -0,0 +1,43 @@
+package com.example.a17255.a8project;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by 17255 on 2017/10/9.
+ */
+
+public class WinDialogFragment extends DialogFragment {
+    Context context;
+    int score;
+
+    public WinDialogFragment(Context context, int score) {
+        this.context = context;
+        this.score = score;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("You Win!")
+                .setPositiveButton("Great!", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(context, HighScore.class);
+                        intent.putExtra("score", score);
+                        startActivity(intent);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+
+
+}
